2114471e2eca975af470b2fbbc11e80cddddc354
Hubert Ryan <hubertryanofficial@gmail.com>
2024-05-22 18:15:25 -0300

    feat: new drawer header

---
+++ src/components/DrawerHeader.tsx
@@ -0,0 +1,26 @@
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
+
+import { DrawerHeaderProps } from "@react-navigation/drawer";
+import { EllipsisVertical, Menu } from "lucide-react-native";
+
+export default function DrawerHeader({
+  navigation,
+  options,
+}: DrawerHeaderProps) {
+  return (
+    <SafeAreaView className="bg-white">
+      <View className="h-[50px] w-full flex-row items-center justify-between px-4">
+        <TouchableOpacity onPress={() => navigation.openDrawer()}>
+          <Menu color="#000000" />
+        </TouchableOpacity>
+        <View className="bg-gray-200 px-4 py-2 rounded-lg">
+          <Text>Get Plus</Text>
+        </View>
+        <TouchableOpacity>
+          <EllipsisVertical color="#000000" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
--- src/routes/DrawerNavigator.tsx
+++ src/routes/DrawerNavigator.tsx
@@ -1,11 +1,16 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import ChatGPT from "../pages/ChatGPT";
+import DrawerHeader from "../components/DrawerHeader";
 
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        header: (props) => <DrawerHeader {...props} />,
+      }}
+    >
       <Drawer.Screen name="ChatGPT" component={ChatGPT} />
     </Drawer.Navigator>
   );
