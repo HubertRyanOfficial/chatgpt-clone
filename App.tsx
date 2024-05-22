775c30ffda5afff653fdd558d79414261ed55c05
Hubert Ryan <hubertryanofficial@gmail.com>
2024-05-21 22:32:33 -0300

    feat: nativewind types and drawer

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 import ChatGPT from "./src/assets/logo.svg";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <ChatGPT width={100} height={100} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
